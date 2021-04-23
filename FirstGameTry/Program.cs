2753c82640a7c5d91ab8cd7601fb110fbc05be9c
Ershov Nikita <@DESKTOP-INLI5BH>
2021-04-23 14:40:06 +0300

    Add Main Class

--- FirstGameTry/Program.cs
+++ FirstGameTry/Program.cs
@@ -10,6 +10,7 @@ namespace FirstGameTry
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ FirstGameTry/Units/MainUnitClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstGameTry.Units
+{
+    class MainUnitClass
+    {
+        public int HP { get; set; }
+        public string Name { get; set; }
+        public int Damage { get; set; }
+        public int Cost { get; set; }
+        public int RadiusAttack { get; set; }
+    }
+}
